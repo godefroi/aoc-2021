3f9439bd99f04abb21e4ca6453ab4212921f7a18
Mark Parker <mparker@medicity.com>
2021-12-06 09:34:43 -0700

    Add day 6 solution

---
+++ day_06/Program.cs
@@ -0,0 +1,29 @@
+﻿using aoc_tools;
+
+var input   = (await PuzzleInput.GetInputLines()).First().Split(',').Select(s => int.Parse(s)).ToList();
+//var input   = "3,4,3,1,2".Split(',').Select(s => int.Parse(s)).ToList();
+var buckets = Enumerable.Range(0, 9).Select(i => input.LongCount(n => n == i)).ToArray();
+
+//Console.WriteLine(input.Count);
+//Console.WriteLine($"Initial state: {string.Join(",", input)}");
+
+for (var day = 0; day < 256; day++) {
+	var nbuckets = new long[9];
+
+	// age everyone one day
+	Array.Copy(buckets, 1, nbuckets, 0, 8);
+
+	// reset everyone who produced a fish
+	nbuckets[6] += buckets[0];
+
+	// add any new fish
+	nbuckets[8] = buckets[0];
+	buckets = nbuckets;
+
+	//Console.WriteLine($"After {day + 1} days: {string.Join(",", buckets)}");
+	if (day == 79) {
+		Console.WriteLine($"part 1: {buckets.Sum()}"); // part 1 is 346063
+	}
+}
+
+Console.WriteLine($"part 2: {buckets.Sum()}"); // part 2 is 1572358335990
