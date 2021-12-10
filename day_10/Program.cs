55b2c80e003c650ed87fb39836b44e7047eb3f6f
Mark Parker <godefroi@gmail.com>
2021-12-10 06:53:11 -0700

    Add solution for day 10

---
+++ day_10/Program.cs
@@ -0,0 +1,53 @@
+﻿using aoc_tools;
+
+//var input = @"[({(<(())[]>[[{[]{<()<>>
+//[(()[<>])]({[<{<<[]>>(
+//{([(<{}[<>[]}>{[]{[(<()>
+//(((({<>}<{<{<>}{[]{[]{}
+//[[<[([]))<([[{}[[()]]]
+//[{[{({}]{}}([{[{{{}}([]
+//{<[[]]>}<{[{[{[]{()[[[]
+//[<(<(<(<{}))><([]([]()
+//<{([([[(<>()){}]>(<<{{
+//<{([{{}}[<[[[<>{}]]]>[]]
+//".Split(Environment.NewLine).SkipLast(1).ToList();
+
+var input = (await PuzzleInput.GetInputLines()).SkipLast(1).ToList();
+
+var starts = new List<char>() { '(', '[', '{', '<' };
+var ends   = new List<char>() { ')', ']', '}', '>' };
+var scores = input.Select(l => ScoreLine(l)).ToList();
+
+Console.WriteLine($"part 1: {scores.Where(s => s > 0).Sum()}"); // part 1 is 296535
+
+scores = scores.Where(s => s < 0).OrderBy(s => s).ToList();
+
+Console.WriteLine($"part 2: {Math.Abs(scores[scores.Count / 2])}"); // part 2 is 4245130838
+
+long ScoreLine(string line)
+{
+	var stack = new Stack<char>();
+
+	for (var pos = 0; pos < line.Length; pos++) {
+		if (starts.Contains(line[pos])) {
+			stack.Push(line[pos]);
+		} else if (ends.Contains(line[pos])) {
+			var c = stack.Pop();
+			if (starts.IndexOf(c) != ends.IndexOf(line[pos])) {
+				//Console.WriteLine($"invalid char at position {pos} for this line: expected {ends[starts.IndexOf(c)]}, found {line[pos]}");
+				return line[pos] switch {
+					')' => 3, ']' => 57, '}' => 1197, '>' => 25137, _ => throw new InvalidOperationException($"Cannot score that character.")
+				};
+			}
+		}
+	}
+
+	var score = 0L;
+
+	foreach (var c in stack) {
+		score *= 5;
+		score += c switch { '(' => 1, '[' => 2, '{' => 3, '<' => 4, _ => throw new InvalidOperationException("No start for that.") };
+	}
+
+	return -score;
+}
