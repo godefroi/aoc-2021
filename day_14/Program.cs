5b4ddf8a6975aecf7fb607fb71a9902c26349578
Mark Parker <godefroi@gmail.com>
2021-12-14 00:02:25 -0700

    This solution to part 2 would take ~150 hours...

--- day_14/Program.cs
+++ day_14/Program.cs
@@ -20,31 +20,54 @@
 //CN -> C
 //".Split(Environment.NewLine).SkipLast(1).ToList();
 
-var input = (await PuzzleInput.GetInputLines()).SkipLast(1).ToList();
+var input   = (await PuzzleInput.GetInputLines()).SkipLast(1).ToList();
+var cmap    = input.Skip(2).SelectMany(l => new[] { l[0], l[1], l[6] }).Distinct().ToList();
+var polymer = input[0].Select(c => cmap.IndexOf(c)).ToList();
+var counts  = new long[cmap.Count];
+var rules   = new int[cmap.Count, cmap.Count];
 
-var polymer = input[0].ToCharArray().ToList();
-var rules   = input.Skip(2).ToDictionary(r => (First: r[0], Second: r[1]), r => r[6]);
-
-for (var i = 0; i < 40; i++) {
-	for (var p = polymer.Count - 1; p > 0; p--) {
-		//Console.WriteLine($"{polymer[p - 1]}{polymer[p]}");
-		if (rules.TryGetValue((First: polymer[p - 1], Second: polymer[p]), out var insert)) {
-			polymer.Insert(p, insert);
-		}
-		//Console.WriteLine(new String(polymer.ToArray()));
-	}
-	//break;
-	Console.WriteLine($"finished step {i} with a chain of length {polymer.Count}");
+foreach (var line in input.Skip(2)) {
+	rules[cmap.IndexOf(line[0]), cmap.IndexOf(line[1])] = cmap.IndexOf(line[6]);
 }
 
-/*Console.WriteLine(polymer.Count);
+//Console.WriteLine($"part 1: {groups.Max(g => g.LongCount()) - groups.Min(g => g.LongCount())}"); // part 1 is 2233
 
-foreach (var g in polymer.GroupBy(c => c)) {
-	Console.WriteLine($"{g.Key} -> {g.Count()}");
-}*/
+var sw = System.Diagnostics.Stopwatch.StartNew();
 
-var groups = polymer.GroupBy(c => c);
+try {
+	foreach (var c in Expand(polymer, rules, 22, 0)) {
+		counts[c]++;
+	}
 
-Console.WriteLine($"part 1: {groups.Max(g => g.LongCount()) - groups.Min(g => g.LongCount())}"); // part 1 is 2233
+	Console.WriteLine(counts.Max() - counts.Min());
+} finally {
+	Console.WriteLine($"Terminated after {sw.Elapsed}");
+}
 
-record struct Rule(char First, char Second, char Insert);
+static IEnumerable<int> Expand(IEnumerable<int> input, int[,] rules, int maxIterations, int iteration = 1)
+{
+	if (iteration >= maxIterations) {
+		foreach (var c in input) {
+			yield return c;
+			Console.WriteLine($"Yielded {c} from input");
+		}
+
+		yield break;
+	}
+
+	var prev = -1;
+
+	foreach (var cur in Expand(input, rules, maxIterations, iteration + 1)) {
+		if (prev == -1) {
+			yield return cur;
+			prev = cur;
+			continue;
+		}
+
+		yield return rules[prev, cur];
+
+		yield return cur;
+
+		prev = cur;
+	}
+}
