12d95ebf0699ae5297433228516ce43c5cd533f1
Mark Parker <mparker@medicity.com>
2021-12-15 09:43:27 -0700

    Add solution via Dijkstra

---
+++ day_15/Dijkstra.cs
@@ -0,0 +1,77 @@
+﻿namespace Dijkstra;
+
+public class Node
+{
+	public int X { get; private set; }
+
+	public int Y { get; private set; }
+
+	public int Risk { get; private set; }
+
+	public int Distance { get; set; } = int.MaxValue;
+
+	public bool Visited { get; set; } = false;
+
+	public Node(int x, int y, int risk)
+	{
+		X    = x;
+		Y    = y;
+		Risk = risk;
+	}
+}
+
+internal static class Dijkstra
+{
+	private static readonly (int x, int y)[] _adjacent = new[] {
+		(x: -1, y:  0),
+		(x:  1, y:  0),
+		(x:  0, y: -1),
+		(x:  0, y:  1)
+	 };
+
+	public static int RunDijkstra(Dictionary<(int X, int Y), Node> graph)
+	{
+		var max = graph.Values.Max(n => n.X);
+
+		return RunDijkstra(graph, graph[(max, max)]);
+	}
+
+	public static int RunDijkstra(Dictionary<(int X, int Y), Node> graph, Node target)
+	{
+		var next = new PriorityQueue<Node, int>();
+
+		graph[(0, 0)].Distance = 0;
+
+		next.Enqueue(graph[(0, 0)], 0);
+
+		while (next.Count > 0) {
+			var cur = next.Dequeue();
+
+			if (cur.Visited) {
+				continue;
+			}
+
+			cur.Visited = true;
+
+			if (cur == target) {
+				return target.Distance;
+			}
+
+			var neighbors = _adjacent.Select(adj => (cur.X + adj.x, cur.Y + adj.y)).Where(coord => graph.ContainsKey(coord) && !graph[coord].Visited).Select(coord => graph[coord]);
+
+			foreach (var neighbor in neighbors) {
+				var alt = cur.Distance + neighbor.Risk;
+
+				if (alt < neighbor.Distance) {
+					neighbor.Distance = alt;
+				}
+
+				if (neighbor.Distance != int.MaxValue) {
+					next.Enqueue(neighbor, neighbor.Distance);
+				}
+			}
+		}
+
+		return target.Distance;
+	}
+}
--- day_15/Program.cs
+++ day_15/Program.cs
@@ -1,105 +1,28 @@
-﻿using Pathfinding.Models;
+﻿using Dijkstra;
 
-using System.Numerics;
+using static Dijkstra.Dijkstra;
 
-var input  = File.ReadAllLines(Path.Combine(Path.GetDirectoryName(args[0]), "input_sample.txt"));
-var cells  = new List<Cell>();
-var width  = input[0].Length;
+var input  = File.ReadAllLines(@"C:\Users\mark.parker\source\repos\aoc-2021\day_15\input.txt");
+var graph  = input.SelectMany((line, y) => line.Select((c, x) => new Node(x, y, int.Parse(c.ToString())))).ToDictionary(n => (n.X, n.Y));
 var height = input.Length;
 
-for (var i = 0; i < height; i++) {
-	for (var j = 0; j < width; j++) {
-		cells.Add(new Cell(j, i, int.Parse(input[i][j].ToString())));
-	}
-}
+Console.WriteLine($"part 1: {RunDijkstra(graph)}");
 
-foreach (var row in cells.GroupBy(c => c.Y).OrderBy(g => g.Key)) {
-	Console.WriteLine(string.Join("", row.OrderBy(c => c.X).Select(c => c.Risk.ToString())));
-}
+graph = graph.Values.SelectMany(n => Scale(n, 5, height)).ToDictionary(n => (n.X, n.Y));
 
-var start   = cells.Single(c => c.X == 0 && c.Y == 0);
-var end     = cells.Single(c => c.X == width - 1 && c.Y == height - 1);
+Console.WriteLine($"part 2: {RunDijkstra(graph)}");
 
-//// find the cost of the most-dumb path
-//var best = cells.Where(c => c.X == 0 || c.Y == height - 1).Sum(c => c.Risk) - start.Risk;
-
-//// now explore all paths, terminating if the path is worse than best
-//static int? Explore(Cell from, List<Cell> cells, int width, int height, int risk, int best, HashSet<Cell> visited) {
-
-//	foreach (var adj in FindAdjacents(from, cells, width, height)) {
-//		// don't re-visit anything we've been to
-//		if (visited.Contains(adj)) {
-//			continue;
-//		}
-
-
-//	}
-//}
-
-//var nodes = new List<List<AStarSharp.Node>>();
-
-//foreach (var row in cells.GroupBy(c => c.Y).OrderBy(g => g.Key)) {
-//	var rlist = new List<AStarSharp.Node>();
-
-//	foreach (var cell in row.OrderBy(c => c.X)) {
-//		rlist.Add(new AStarSharp.Node(new Vector2(cell.X, cell.Y), true, cell.Risk));
-//	}
-
-//	nodes.Add(rlist);
-//}
-
-//var astar = new AStarSharp.Astar(nodes);
-
-//var path = astar.FindPath(new Vector2(0, 0), new Vector2(width - 1, height - 1));
-
-//foreach (var n in path) {
-//	Console.WriteLine($"{n.Position.X},{n.Position.Y} ({n.Weight})");
-//}
-
-//Console.WriteLine(path.Sum(n => n.Weight));
-
-var path = Pathfinding.Pathfinder.FindPath(new MyGrid(cells), new Position(0, 0), new Position(9, 9), Pathfinding.Types.DiagonalMovement.NEVER);
-
-foreach (var p in path) {
-	Console.WriteLine($"{p.X},{p.Y}");
-}
-
-static IEnumerable<Cell> FindAdjacents(Cell current, List<Cell> cells, int width, int height)
+static IEnumerable<Node> Scale(Node node, int factor, int width)
 {
-	// above
-	if (current.X > 0) {
-		yield return cells.Single(c => c.X == current.X - 1 && c.Y == current.Y);
-	}
+	for (var x = 0; x < factor; x++) {
+		for (var y = 0; y < factor; y++) {
+			var nrisk = node.Risk + x + y;
 
-	// left
-	if (current.Y > 0) {
-		yield return cells.Single(c => c.Y == current.Y - 1 && c.X == current.X);
-	}
+			while (nrisk > 9) {
+				nrisk -= 9;
+			}
 
-	// below
-	if (current.X < height - 1) {
-		yield return cells.Single(c => c.X == current.X + 1 && c.Y == current.Y);
-	}
-
-	// right
-	if (current.Y < width - 1) {
-		yield return cells.Single(c => c.Y == current.Y + 1 && c.X == current.X);
-	}
-}
-
-record struct Cell(int X, int Y, int Risk);
-
-class MyGrid : Pathfinding.Models.BaseGrid
-{
-	private List<Cell> _cells;
-
-	public MyGrid(List<Cell> cells) : base(cells.Max(c => c.X + 1), cells.Max(c => c.Y + 1))
-	{
-		_cells = cells;
-	}
-
-	public override bool IsWalkableAt(Position p, bool final, params object[] args)
-	{
-		return true;
+			yield return new Node(node.Y + (width * y), node.X + (width * x), nrisk);
+		}
 	}
 }
